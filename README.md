7f4b8fc36cd8666e8dd35a7e7bce8ba86e54d823
FlexiEndpoints <81296586+s34azuread@users.noreply.github.com>
2024-10-31 05:43:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# DEPLOYMENT
+
+## INSTALLATION
+
+1 
+```commandline
+pip install -r requirements.txt
+```
+
+## SERVICE STARTUP
+
+1
+```commandline
+uvicorn main:app --reload --host 0.0.0.0 --port 8000
+```
+
+## SWAGGER URL
+
+```commandline
+
+http://localhost:8000/docs
+
+OR
+
+http://<server ip or domain name>:8000/docs
+
+```
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+# app.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World v5 final from Docker"}
+
+# Allow requests from localhost (React frontend)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update with React frontend URL if deployed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/data")
+async def get_data():
+    return {"message": "Hello from FastAPI Backend!"}
